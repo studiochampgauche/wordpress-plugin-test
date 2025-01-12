3b53d0b28f742320c1820ad30a39d1b266b5742b
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-12 08:01:22 -0500

    Create init.php

---
+++ my-plugin/init.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+* Plugin Name: My plugin
+* Description: My description
+* Version: 1.0.1
+* Author: Studio Champ Gauche
+* Author URI: https://champgauche.studio
+* Plugin URI: false
+*/
